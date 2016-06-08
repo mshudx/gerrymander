6048560c93c85b0ebd27c1fb0440fcf5b3573bdc
Balint Farkas <balint.farkas@windowslive.com>
2016-06-08 12:53:42 +0200

    VotingSite can now be called via Remoting to store a vote.

---
+++ Gerrymander/Gerrymander.ServiceFabric.VotingSite/IVotingSite.cs
@@ -0,0 +1,15 @@
+﻿using Gerrymander.Common;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerrymander.ServiceFabric.VotingSite
+{
+    public interface IVotingSite : IService
+    {
+        Task StoreVote(Vote vote);
+    }
+}
--- Gerrymander/Gerrymander.ServiceFabric.VotingSite/VotingSite.cs
+++ Gerrymander/Gerrymander.ServiceFabric.VotingSite/VotingSite.cs
@@ -4,20 +4,31 @@ using System.Fabric;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Gerrymander.Common;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 
 namespace Gerrymander.ServiceFabric.VotingSite
 {
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class VotingSite : StatefulService
+    internal sealed class VotingSite : StatefulService, IVotingSite
     {
+        private const string storedVotesDictionaryName = "storedVotes";
+        private IReliableDictionary<string, Vote> storedVotes;
+
         public VotingSite(StatefulServiceContext context)
             : base(context)
         { }
+
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            storedVotes = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, Vote>>(storedVotesDictionaryName);
+            await base.RunAsync(cancellationToken);
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -28,41 +39,23 @@ namespace Gerrymander.ServiceFabric.VotingSite
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            return new[] { new ServiceReplicaListener(context => this.CreateServiceRemotingListener(context)) };
         }
 
         /// <summary>
-        /// This is the main entry point for your service replica.
-        /// This method executes when this replica of your service becomes primary and has write status.
+        /// This method can be called remotely via Service Remoting.
         /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
+        /// <param name="vote"></param>
+        /// <returns></returns>
+        public async Task StoreVote(Vote vote)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
-
-            while (true)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                using (var tx = this.StateManager.CreateTransaction())
-                {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
-
-                    ServiceEventSource.Current.ServiceMessage(this, "Current Counter Value: {0}",
-                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
-
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
-
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-            }
+            var transaction = this.StateManager.CreateTransaction();
+            await storedVotes.AddOrUpdateAsync(
+                transaction,
+                vote.VoteId.ToString(),
+                vote,
+                (_, __) => { return vote; });
+            await transaction.CommitAsync();
         }
     }
 }
