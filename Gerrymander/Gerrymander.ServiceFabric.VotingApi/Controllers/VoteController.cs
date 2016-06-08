b8ea967dbbbd7992c68e5cb3b0a3047da313ff15
Balint Farkas <balint.farkas@windowslive.com>
2016-06-08 11:55:20 +0200

    Added Ballot Box Actor which implements ballot box open/close, and vote validation. Voting API passes votes to an actor.

--- Gerrymander/Gerrymander.ServiceFabric.VotingApi/Controllers/VoteController.cs
+++ Gerrymander/Gerrymander.ServiceFabric.VotingApi/Controllers/VoteController.cs
@@ -1,8 +1,13 @@
 ﻿using Gerrymander.Common;
+using Gerrymander.ServiceFabric.BallotBoxActor.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -11,14 +16,31 @@ namespace Gerrymander.ServiceFabric.VotingApi.Controllers
 {
     public class VoteController : ApiController
     {
-        public string Get(string value)
+        public string Get()
         {
-            return "Echo: " + value;
+            return "Voting API is online (" + DateTime.Now.ToString() + "). Use HTTP POST to send a vote.";
         }
 
-        public string Post([FromBody]Vote vote)
+        public async Task<HttpResponseMessage> Post([FromBody]Vote vote)
         {
-            return "Received vote for " + vote.Candidate + ". Excellent choice!";
+            // Received vote; redirecting it to the appropriate ballot box actor
+            ActorId actorId = new ActorId(vote.BallotBoxId);
+            IBallotBoxActor ballotBoxActor =
+                ActorProxy.Create<IBallotBoxActor>(
+                    actorId,
+                    new Uri("fabric:/Gerrymander.ServiceFabric/BallotBoxActorService"));
+
+            // TODO open ballot boxes when the election is opened
+            await ballotBoxActor.OpenBallotBoxAsync();
+            var voteResult = await ballotBoxActor.SubmitVoteAsync(vote);
+            if (voteResult == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, voteResult);
+            }
         }
     }
 }
